afef77fd8dc7c5e574fc67363189c9b360839b01
jinyoung804 <elin090804@gmail.com>
2026-08-03 00:35:04 +0100

    지도 축소 화면 마커 조정 가게 추가

--- lib/Explore/services/place_service.dart
+++ lib/Explore/services/place_service.dart
@@ -1,13 +1,18 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class PlaceService {
-  final SupabaseClient _supabase = Supabase.instance.client;
+  final SupabaseClient _supabase =
+      Supabase.instance.client;
 
-  Future<List<Map<String, dynamic>>> getPlaces() async {
+  Future<List<Map<String, dynamic>>>
+  getPlaces() async {
     final response = await _supabase
         .from('places')
-        .select();
+        .select()
+        .order('id');
 
-    return List<Map<String, dynamic>>.from(response);
+    return List<Map<String, dynamic>>.from(
+      response,
+    );
   }
 }
