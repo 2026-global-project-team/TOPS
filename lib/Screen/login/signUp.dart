a532786011df21b7ba55c6d136837200c232ad01
JeongMo <jojunhg5@gmail.com>
2026-08-03 11:55:30 +0100

    loginPage Complete And I have been working signUpPage implementing

---
+++ lib/Screen/login/signUp.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget{
+  const SignUpPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _signPageState();
+}
+
+class _signPageState extends State<SignUpPage>{
+
+  @override
+  Widget build(BuildContext context) {
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-import 'camera_test_page.dart';
+import 'package:tops/Screen/login/login.dart';
 
 late List<CameraDescription> cameras;
 
@@ -10,10 +10,17 @@ Future<void> main() async {
 
   cameras = await availableCameras();
 
-  runApp(
-    MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CameraTestPage(camera: cameras.first),
-    ),
-  );
+      home: LoginPage(),
+    );
+  }
 }
